da49b72d6f153db6216a17a7d1b50ba59d5a0af5
Adrian Kyzer <adriankyzer@gmail.com>
2024-10-31 11:10:29 +0100

    add tip tap and prose mirrir dependencies and basic text editor

--- src/app/components/hashtag-input/hashtag-input.component.ts
+++ src/app/components/hashtag-input/hashtag-input.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Editor } from '@tiptap/core';
+import StarterKit from '@tiptap/starter-kit';
 
 @Component({
   selector: 'app-hashtag-input',
   standalone: true,
   imports: [],
   templateUrl: './hashtag-input.component.html',
-  styleUrl: './hashtag-input.component.css'
+  styleUrl: './hashtag-input.component.css',
 })
-export class HashtagInputComponent {
+export class HashtagInputComponent implements OnInit, OnDestroy {
+  private editor: Editor;
 
+  ngOnInit() {
+    this.editor = new Editor({
+      element: document.querySelector('.editor-content')!,
+      extensions: [StarterKit],
+      editorProps: {
+        attributes: {
+          class: 'prose prose-sm sm:prose lg:prose-lg xl:prose-2xl mx-auto focus:outline-none',
+        },
+      },
+    });
+  }
+
+  ngOnDestroy() {
+    this.editor.destroy();
+  }
 }
